2c3c6e3062839869017b5b18d0c51f37feff26eb
kamau simon <kamausimon217@gmail.com>
2025-10-25 13:51:59 +0300

    Add unit tests for CleanInput function to validate input processing

--- repl_test.go
+++ repl_test.go
@@ -1,1 +1,45 @@
 package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    " Hello World ",
+			expected: []string{"hello", "world"},
+		},
+
+		{
+			input:    " Hello doug",
+			expected: []string{"hello", "doug"},
+		},
+		{
+			input:    "Hey Kamau ",
+			expected: []string{"hey", "kamau"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := CleanInput(c.input)
+		if len(actual) != len(c.expected) {
+			t.Errorf("the lengths don't match")
+			return
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("words don't match")
+				return
+			}
+		}
+	}
+
+}
